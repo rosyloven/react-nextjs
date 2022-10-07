49c7d2009a2c0890bd7c5ec82517d04457ac4118
Богдан Чайка <bogdancajka@MacBook-Pro-Bogdan.local>
2022-10-07 11:02:52 +0300

    changes

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+const Index = () =>
+  <Link href="/pokemons">
+    <a>Go To Pokemons Page</a>
+  </Link>
+
+export default Index;
---
+++ pages/pokemons.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+const Pokemons = () =>
+    <Link href="/">
+      <a>Go To Home Page</a>
+    </Link>
+
+export default Pokemons;
